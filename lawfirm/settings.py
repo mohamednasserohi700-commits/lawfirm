d23f53a13d028d3cfa36a656ff62598e0a40d19e
mohamednasserohi700-commits <mohamednasserohi700@icloud.com>
2026-04-28 19:46:02 +0300

    Add files via upload

--- lawfirm/settings.py
+++ lawfirm/settings.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.users.user_activity_middleware.UserActivityMiddleware',
 ]
 
 ROOT_URLCONF = 'lawfirm.urls'
